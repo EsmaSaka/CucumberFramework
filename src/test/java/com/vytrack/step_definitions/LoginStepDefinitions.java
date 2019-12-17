6a8a3f52e828edf5a8c87eafed3781a7b60cb67b
esma <esmasaka5@gmail.com>
2019-12-17 14:13:19 -0500

    implement some code before hook

--- src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefinitions.java
@@ -7,31 +7,28 @@ public class LoginStepDefinitions {
     @Given("user is on the login page")
     public void user_is_on_the_login_page() {
         System.out.println("i am on the login page");
-//        throw new cucumber.api.PendingException();
+
     }
 
     @Then("user logs in as  store manager")
     public void user_logs_in_as_store_manager() {
-        // Write code here that turns the phrase above into concrete actions
-       // throw new cucumber.api.PendingException();
+
         System.out.println("store Manager");
     }
 
     @Then("user verifies that {string} page subtitle is displayed")
     public void user_verifies_that_page_subtitle_is_displayed(String string) {
-        System.out.println("verifying page sun title"+string);
-     //   throw new cucumber.api.PendingException();
+        System.out.println("verifying page sun title  "+string);
+
     }
     @Then("user logs in as  driver")
     public void user_logs_in_as_driver() {
-        // Write code here that turns the phrase above into concrete actions
-      //  throw new cucumber.api.PendingException();
+
         System.out.println("driver");
     }
     @Then("user logs in as sales manager")
     public void user_logs_in_as_sales_manager() {
-        // Write code here that turns the phrase above into concrete actions
-       // throw new cucumber.api.PendingException();
+
         System.out.println("logged in as sales manager");
     }
 
