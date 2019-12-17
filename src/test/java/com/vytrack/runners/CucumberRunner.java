d6dcc3d9932c3281a09bb62b1726f91cdfe8deb5
esma <esmasaka5@gmail.com>
2019-12-17 11:19:57 -0500

    Added cucmberRunnner trigger

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -1,4 +1,9 @@
 package com.vytrack.runners;
 
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)// comes from junit cucumber doeas not have an engine
 public class CucumberRunner {
+
 }
