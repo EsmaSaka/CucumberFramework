9d1f2abcfb5051b4e010f35f914aa7fbd8682df7
esma <esmasaka5@gmail.com>
2019-12-17 15:32:28 -0500

    tilde

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -6,8 +6,10 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)// comes from junit cucumber doeas not have an engine
 @CucumberOptions(features = "src\\test\\resources\\features",
-        glue ="com\\vytrack\\step_definitions",dryRun = false,
-        tags = "@negative_test")// checks if code implementations are done flase by default
+        glue ="com\\vytrack\\step_definitions",dryRun = false)
+       // tags = " not or ~@negative_test")// checks if code implementations are done flase by default
+        // ~ tilde
+        // tags="~@negative_test" runs except this tag name old way
 
 public class CucumberRunner {
 
