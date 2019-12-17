7c9ab3ddb91f7ce434c34b405c7a3adf2e73fe59
esma <esmasaka5@gmail.com>
2019-12-17 11:36:28 -0500

    Added cucmberRunnner gitignore

--- src/test/java/com/vytrack/runners/CucumberRunner.java
+++ src/test/java/com/vytrack/runners/CucumberRunner.java
@@ -1,9 +1,11 @@
 package com.vytrack.runners;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)// comes from junit cucumber doeas not have an engine
+@CucumberOptions(features = "src\\test\\resources\\features")
 public class CucumberRunner {
 
 }
